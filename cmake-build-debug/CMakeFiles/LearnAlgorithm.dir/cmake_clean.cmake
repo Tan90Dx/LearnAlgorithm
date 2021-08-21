beb019a91209e901c3843d7f5becbeb499240dd2
“Freeman” <tan90dx@gmail.com>
2021-08-21 11:48:36 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/LearnAlgorithm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnAlgorithm.dir/main.cpp.obj"
+  "LearnAlgorithm.exe"
+  "LearnAlgorithm.exe.manifest"
+  "LearnAlgorithm.pdb"
+  "libLearnAlgorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearnAlgorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
